c36ab7a2db394480ca5613769a2e4ecd86ffbc1a
Mahesh <maheshchabukswar1@gmail.com>
2023-04-04 13:07:35 +0530

    2023 files

---
+++ src/pkg2/Demo.java
@@ -0,0 +1,11 @@
+package pkg2;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int i = 5;		
+		System.out.println("Digit is " + i);
+
+	}
+
+}
---
+++ src/pkg2/Hello.java
@@ -0,0 +1,11 @@
+package pkg2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	This is my fist pogam and 
+	i want to be bette at it
+
+	}
+
+}
